6e9b456127781c1f531759a86e41cb385652efc5
Angel Armendariz <angela126509@gmail.com>
2023-03-01 18:23:16 -0800

    environment set up

--- SetUpEnvTest.py
+++ SetUpEnvTest.py
@@ -3,18 +3,18 @@ from flask import Flask
 from flask_mysqldb import MySQL
 app = Flask(__name__)
 app.config['MYSQL_USER'] = 'root' 
-app.config['MYSQL_PASSWORD'] = 'QggmEjMsqy'
+app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_HOST'] = '127.0.0.1'
-app.config['MYSQL_PORT'] = '3306'
-app.config['MYSQL_DB'] = '127.0.0.1'
+app.config['MYSQL_PORT'] = 3306
+app.config['MYSQL_DB'] = 'cpsc431_armendarizangel'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 mysql = MySQL(app)
 
 
 @app.route('/')
-def users():
+def index():
     cur = mysql.connection.cursor()
-    cur.execute('''SELECT user, host FROM mysql.user''')
+    cur.execute('''SELECT * FROM cpsc431_armendarizangel.pet''')
     rv = cur.fetchall()
     return str(rv)
 
